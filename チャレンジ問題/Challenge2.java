d9334700b6ee9bcf0482e0257d63c6ca9d3e62d6
長岡良平 <ryohei.nagaoka@seattleconsulting.co.jp>
2018-12-13 15:17:48 +0900

    ２問目完了

--- チャレンジ問題/Challenge2.java
+++ チャレンジ問題/Challenge2.java
@@ -1,3 +1,25 @@
+/**
+* ピラミッド作成ゲーム
+* @author nagaoka
+*/
+
+
 public class Challenge2{
-	public static void main(String[] args)
+	/**
+	*ピラミッド型の文字列を出力する
+	*/
+	public static void main(String[] args){
+
+		//ピラミッドを列毎に設定
+		String row1 = "      ■      ";
+		String row2 = "    ■   ■    ";
+		String row3 = "  ■ ■ ■ ■ ■  ";
+		String row4 = "■ ■ ■ ■ ■ ■ ■";
+		//列毎に入れた変数を上から順に配列に格納
+		String[] pyramids ={row1,row2,row3,row4};
+
+		for(String pyramid:pyramids){
+			System.out.println(pyramid);
+		}
+	}
 }
